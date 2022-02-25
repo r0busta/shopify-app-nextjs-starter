b84093713d1bd6b5a5f1b14495df5cfbb54cd529
Kirill Zhuchkov <643378+r0busta@users.noreply.github.com>
2022-02-25 18:08:55 +0100

    Move app related stuff to a separate NPM package

--- src/lib/token.ts
+++
@@ -1,7 +0,0 @@
-export const parseJwt = (token: string) => {
-    try {
-        return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString())
-    } catch (e) {
-        return null
-    }
-}
---
+++ src/pages/api/shopify/[...action].ts
@@ -0,0 +1,3 @@
+import { requireAppRoutes } from "next-shopify-public-app"
+
+export default requireAppRoutes()
